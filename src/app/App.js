0e1e3942fede82e43258df6623d95fded0c5ad30
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-05-04 09:30:15 -0400

    add basic file structure for rendering poke items

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,16 @@
 import { Component } from 'react';
 import './App.css';
 import React from 'react';
+import Header from './Header';
+import PokeList from '../pokemon/PokeList';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-  
-        My React App...
-        <img src="acl-logo.png" className="temp-images" alt="acl logo" />
-      
+        <Header />
+        <PokeList />
       </div>
     );
   }
